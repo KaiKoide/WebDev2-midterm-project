edf5e9460ad0cb4b520ebeda2e5b53a351e3bd8f
Kai <kkai.chien0813@gmail.com>
2023-11-25 19:12:10 -0800

    :wrench: Introduce a CSS framework

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/header.webp')"
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
