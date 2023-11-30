52f4ea9a41a0c2b6eb3e4e88befb5085fd2abf5d
Kai <kkai.chien0813@gmail.com>
2023-11-30 11:24:53 -0800

    :bug: Update the quantity of product

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-// import { useState } from 'react'
-// import './App.css'
-import React, { useState, createContext } from 'react'
-import ReactDOM from 'react-dom/client'
-import HeroSection from './components/HeroSection'
-import MenuSection from './components/MenuSection'
+import React, { useState, createContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import HeroSection from './components/HeroSection';
+import MenuSection from './components/MenuSection';
 
 export const ProductArray = createContext();
 
 function App() {
-
-  // const [cartsVisibility, setCartsVisibility] = useState(false);
   const [cartProducts, setCartProducts] = useState([]);
 
   // console.log("cartProducts", cartProducts)
 
   const updateCart = (product) => {
-    // console.log("Product", product);
-    // console.log("cartProducts",cartProducts)
-    const fn = cartProducts.find((el) => el.title === product.title);
-     console.log('fn', fn);
-    if(fn){
-     console.log('true');
-     setCartProducts((prev) => {
-      return [...prev,
-        {
-          ...fn,
-          qty: fn.qty + 1
-        }
-      ]
-    });
+    const existingProduct = cartProducts.find((el) => el.id === product.id);
+    console.log('existingProduct', existingProduct);
+    if(existingProduct){
+      setCartProducts((prev) => {
+        return prev.map((item) =>
+          item.id === existingProduct.id
+            ? { ...item, qty: item.qty + 1 }
+            : item
+        );
+      });
     } else {
       setCartProducts((prev) => {
         return [...prev, product]
       });
     }
   }
+  // console.log(cartProducts);
 
   const value = {
     cartProducts,
